089f27971608ddc5af1df65919ab15a800ca0638
reborn <lamponhq123@gmail.com>
2026-05-02 23:25:41 +0700

    ASDASD

--- scripts/deploy-to-master.sh
+++ scripts/deploy-to-master.sh
@@ -8,7 +8,7 @@ MASTER_IP="${1:-localhost}"
 SSH_KEY_PATH="${2:-$HOME/.ssh/id_rsa}"
 SSH_USER="cassandra"
 #REPO_URL="https://github.com/your-org/cis-cassandra-main.git"  # Update this
-EPO_URL="https://github.com/Triggermetric/test-cis"  # Update this
+REPO_URL="https://github.com/Triggermetric/test-cis"  # Update this
 
 echo "Deploying CIS Cassandra to Master VM"
 echo "========================================"
